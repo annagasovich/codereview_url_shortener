4d6ecb4b3e9b0181a7804df3360785b8806311a1
undefeatedsando <thekaidaten@gmail.com>
2022-01-26 22:09:55 +0300

    first blood

---
+++ url_shortener/src/Entity/Link.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LinkRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=LinkRepository::class)
+ */
+class Link
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $long_url;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $short_url;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="array", nullable=true)
+     */
+    private $tags = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLongUrl(): ?string
+    {
+        return $this->long_url;
+    }
+
+    public function setLongUrl(string $long_url): self
+    {
+        $this->long_url = $long_url;
+
+        return $this;
+    }
+
+    public function getShortUrl(): ?string
+    {
+        return $this->short_url;
+    }
+
+    public function setShortUrl(string $short_url): self
+    {
+        $this->short_url = $short_url;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getTags(): ?array
+    {
+        return $this->tags;
+    }
+
+    public function setTags(?array $tags): self
+    {
+        $this->tags = $tags;
+
+        return $this;
+    }
+}
